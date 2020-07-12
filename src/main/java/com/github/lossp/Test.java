2fed38aa63439cbec305feb23214e3fa0529ddc5
lossp <richard.xxmxx@gmail.com>
2020-07-12 23:49:09 +0800

    add the basic controller of the crawler, whose purpose is to initialize the app and get the web information

--- src/main/java/com/github/lossp/Test.java
+++ src/main/java/com/github/lossp/Test.java
@@ -1,4 +1,25 @@
 package com.github.lossp;
 
+import org.apache.http.HttpEntity;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+
 public class Test {
+    public static void main(String[] args) throws IOException{
+        // CloseableHttpClient和HttpClient???
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://www.baidu.com");
+        try(CloseableHttpResponse response1 = httpClient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity httpEntity1 = response1.getEntity();
+            System.out.println(EntityUtils.toString(httpEntity1));
+        }
+    }
 }
