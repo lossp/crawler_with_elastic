47d6865eea7bb8d3f2f5eab7ff0a022504c14e19
lossp <richard.xxmxx@gmail.com>
2020-07-19 15:44:36 +0800

    add zhihu followers crawler unit

---
+++ src/main/java/com/github/lossp/processUnit/ZhihuFollowerCrawlerUnit.java
@@ -0,0 +1,10 @@
+package com.github.lossp.processUnit;
+
+import java.util.concurrent.Callable;
+
+public class ZhihuFollowerCrawlerUnit<T> implements Callable<T> {
+    @Override
+    public T call() {
+        return null;
+    }
+}
