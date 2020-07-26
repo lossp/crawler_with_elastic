67227db6f805416638c2d7956ddf33dbc4116408
lossp <richard.xxmxx@gmail.com>
2020-07-26 23:05:40 +0800

    finishing up the constructor of ZhihuFollwerCrawlerUnit

--- src/main/java/com/github/lossp/processUnit/ZhihuFollowerCrawlerUnit.java
+++ src/main/java/com/github/lossp/processUnit/ZhihuFollowerCrawlerUnit.java
@@ -1,12 +1,27 @@
 package com.github.lossp.processUnit;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.concurrent.Callable;
 
 public class ZhihuFollowerCrawlerUnit<T> implements Callable<T> {
 
-    private final static String FOLLOWER_LIST_API = "https://www.zhihu.com/api/v4/members/excited-vczh/followers?limit=10&offset=10";
-    private int limit;
-    private int offset;
+    private final static String FOLLOWER_LIST_API = "https://www.zhihu.com/api/v4/members/excited-vczh/followers";
+    private final static String LIMIT = "limit";
+    private final static String OFFSET = "offset";
+    private final int limit;
+    private final int offset;
+    private final String taskUrl;
+
+
+    public ZhihuFollowerCrawlerUnit(int limit, int offset) {
+        this.limit = limit;
+        this.offset = offset;
+        // TODO use annotation to improve
+        this.taskUrl = FOLLOWER_LIST_API + "?" + LIMIT + "=" + limit + "&" + OFFSET + "=" + offset;
+    }
+
+
     @Override
     public T call() {
         return null;
