9c0edf19a3115c480db3610ac9facdea8ea1c49a
lossp <richard.xxmxx@gmail.com>
2020-07-26 23:58:05 +0800

    implement the basic code, crawler can fetch followers data

--- src/main/java/com/github/lossp/processUnit/ZhihuFollowerCrawlerUnit.java
+++ src/main/java/com/github/lossp/processUnit/ZhihuFollowerCrawlerUnit.java
@@ -1,8 +1,13 @@
 package com.github.lossp.processUnit;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.concurrent.Callable;
+import com.github.lossp.utils.HttpRequestBuilder;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.concurrent.*;
 
 public class ZhihuFollowerCrawlerUnit<T> implements Callable<T> {
 
@@ -12,18 +17,37 @@ public class ZhihuFollowerCrawlerUnit<T> implements Callable<T> {
     private final int limit;
     private final int offset;
     private final String taskUrl;
+    private HttpRequestBuilder httpRequestBuilder = new HttpRequestBuilder();
+    private final OkHttpClient okHttpClient;
 
 
     public ZhihuFollowerCrawlerUnit(int limit, int offset) {
         this.limit = limit;
         this.offset = offset;
+        this.okHttpClient = new OkHttpClient();
         // TODO use annotation to improve
         this.taskUrl = FOLLOWER_LIST_API + "?" + LIMIT + "=" + limit + "&" + OFFSET + "=" + offset;
     }
 
-
     @Override
     public T call() {
-        return null;
+        try {
+            final Request request = httpRequestBuilder.getRequestBuilder(this.taskUrl);
+            Call call = okHttpClient.newCall(request);
+            Response response = call.execute();
+            String body = response.body().string();
+            System.out.println(body);
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        ZhihuFollowerCrawlerUnit zhihuFollowerCrawlerUnit = new ZhihuFollowerCrawlerUnit(20, 10);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        FutureTask future = new FutureTask(zhihuFollowerCrawlerUnit);
+        executorService.submit(future);
     }
 }
