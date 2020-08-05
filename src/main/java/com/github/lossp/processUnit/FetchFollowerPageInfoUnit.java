74507a4845779b4977b67217af207c4e384a9403
mengfh <mengfh@7moor.com>
2020-08-05 23:44:35 +0800

    add a class to extract info from follower page

---
+++ src/main/java/com/github/lossp/processUnit/FetchFollowerPageInfoUnit.java
@@ -0,0 +1,12 @@
+package com.github.lossp.processUnit;
+
+import com.github.lossp.valueObject.ZhihuUser;
+
+import java.util.concurrent.Callable;
+
+public class FetchFollowerPageInfoUnit implements Callable<ZhihuUser> {
+    @Override
+    public ZhihuUser call() {
+        return null;
+    }
+}
