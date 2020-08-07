c14515e49318f02aeaa00a63da49336c4ee402d2
lossp <richard.xxmxx@gmail.com>
2020-08-07 23:15:01 +0800

    add personal page descriptor selector

--- src/main/java/com/github/lossp/constants/CssClassNameSelector.java
+++ src/main/java/com/github/lossp/constants/CssClassNameSelector.java
@@ -3,6 +3,11 @@ package com.github.lossp.constants;
 public final class CssClassNameSelector {
     private static final String USER_LINK_CLASS_NAME = ".UserLink-link";
     private static final String PAGE_NUMBER_CLASS_NAME = ".Pagination>button";
+    private static final String USER_PAGE_PROFILE = ".ProfileHeader-contentBody";
+
+    private static String getUserPageProfile() {
+        return USER_PAGE_PROFILE;
+    }
 
     public static String getUserLinkClassName() {
         return USER_LINK_CLASS_NAME;
