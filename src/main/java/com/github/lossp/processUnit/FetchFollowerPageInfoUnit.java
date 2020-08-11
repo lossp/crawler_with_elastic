379b9db1cbab671144f6fdae6e8d8abd2b8ce11a
lossp <richard.xxmxx@gmail.com>
2020-08-11 22:37:53 +0800

    gather information from personal page, but part information are lost

--- src/main/java/com/github/lossp/processUnit/FetchFollowerPageInfoUnit.java
+++ src/main/java/com/github/lossp/processUnit/FetchFollowerPageInfoUnit.java
@@ -1,12 +1,48 @@
 package com.github.lossp.processUnit;
 
+import com.github.lossp.constants.CssClassNameSelector;
+import com.github.lossp.factory.LinksPool;
+import com.github.lossp.factory.PageLinksPool;
+import com.github.lossp.valueObject.URLNode;
 import com.github.lossp.valueObject.ZhihuUser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-import java.util.concurrent.Callable;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class FetchFollowerPageInfoUnit implements Callable<ZhihuUser> {
+public class FetchFollowerPageInfoUnit extends AbstractCrawlerUnit<ZhihuUser> {
+    private static String userPageInfoClassName = CssClassNameSelector.getUserPageProfile();
+
+    public FetchFollowerPageInfoUnit(LinksPool linksPool) {
+        super(linksPool);
+    }
+
     @Override
     public ZhihuUser call() {
+        while (!pageLinksPool.isEmpty()) {
+            try {
+                URLNode urlNode = pageLinksPool.poll();
+                System.out.println("===========" + urlNode.getUrl());
+                Document document = Jsoup.connect(urlNode.getUrl()).get();
+                // TODO still, information lost 
+                Elements elements = document.select(userPageInfoClassName);
+                System.out.println(document);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
+    }
+
+    public static void main(String[] args) throws Exception{
+        LinksPool linksPool = PageLinksPool.getInstance();
+        linksPool.put(new URLNode("https", "https://www.zhihu.com/people/tian-yang-yu-14", "GET"));
+        FetchFollowerPageInfoUnit fetchFollowerPageInfoUnit = new FetchFollowerPageInfoUnit(linksPool);
+        ExecutorService executors = Executors.newFixedThreadPool(1);
+        executors.submit(fetchFollowerPageInfoUnit);
+
     }
 }
